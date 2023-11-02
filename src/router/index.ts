92045728ddb7eb1463e5552fa8b3d5a524438841
Jonas Pfeifer <jonaspfeifer@drei.at>
2023-11-02 23:51:18 +0100

    added registry functionality

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import SelectView from "@/views/SelectView.vue";
+import PayoutView from "@/views/PayoutView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: "/select_host",
       name: "select_host",
       component: SelectView
+    },
+    {
+      path: "/payout",
+      name: "payout",
+      component: PayoutView
     }
   ]
 })
---
+++ src/views/PayoutView.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import {io} from "socket.io-client";
+
+const code = randomString(10);
+console.log(code);
+
+const socket = io("https://localhost:8000/", {
+    rejectUnauthorized: false
+});
+socket.on("error", (error: string) => {
+    console.log("Error occurred: " + error);
+})
+socket.on("requested", (userID: string) => {
+    console.log("Requested by: " + userID);
+})
+
+socket.emit("register", code);
+
+function randomString(len: number): string {
+    return Math.random().toString(36).slice(2, len+2);
+}
+</script>
+
+<template>
+    <h1>Payout</h1>
+</template>
+
+<style scoped lang="scss">
+
+</style>
