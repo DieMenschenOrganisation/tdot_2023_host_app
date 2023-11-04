9b95dc366661d527139e582a5d2960f9cebef321
Jonas Pfeifer <jonaspfeifer@drei.at>
2023-11-04 22:44:58 +0100

    added games selection

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import SelectView from "@/views/SelectView.vue";
 import PayoutView from "@/views/PayoutView.vue";
+import GamesView from "@/views/GamesView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: "/payout",
       name: "payout",
       component: PayoutView
+    },
+    {
+      path: "/games",
+      name: "games",
+      component: GamesView
     }
   ]
 })
---
+++ src/views/GamesView.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+function toSlots() {
+    // TODO Redirect to slots
+}
+
+function toHorseRace() {
+    // TODO Redirect to horse race
+}
+</script>
+
+<template>
+    <h1 class="w-100 p-4 text-center">Gewünschtes Spiel auswählen:</h1>
+    <div class="position-absolute top-50 start-50 translate-middle d-flex flex-column align-items-center gap-5">
+        <h3 type="button" class="option" @click="toSlots">Slots</h3>
+        <h3 type="button" class="option" @click="toHorseRace">Pferderennen</h3>
+    </div>
+</template>
+
+<style scoped lang="scss">
+@import "@/assets/variables";
+.option {
+    &:hover {
+        color: $dark-mode-blue;
+    }
+}
+</style>
