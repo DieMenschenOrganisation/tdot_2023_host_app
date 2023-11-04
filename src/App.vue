c5f9d4fa6bff1263a05f29d27876ad232cd24bd5
Jonas Pfeifer <jonaspfeifer@drei.at>
2023-11-04 18:19:16 +0100

    added backendip

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,7 @@
 <script setup lang="ts">
+import {provide} from "vue";
+
+provide("backendIP", "https://localhost:8000/");
 </script>
 
 <template>
--- src/views/PayoutView.vue
+++ src/views/PayoutView.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts">
 import {io} from "socket.io-client";
+import {inject} from "vue";
+import axios from "axios";
+
+const backendIP = inject("backendIP");
 
 const code = randomString(10);
 console.log(code);
@@ -12,19 +16,28 @@ socket.on("error", (error: string) => {
 })
 socket.on("requested", (userID: string) => {
     console.log("Requested by: " + userID);
+    axios.get(backendIP + "user/points/" + userID).then(points => {
+        console.log("User has " + points.data + " points!");
+    }).catch(error => {
+        console.log("Error: " + error.reason);
+    })
 })
 
 socket.emit("register", code);
 
 function randomString(len: number): string {
-    return Math.random().toString(36).slice(2, len+2);
+    return Math.random().toString(36).slice(2, len + 2);
 }
 </script>
 
 <template>
-    <h1>Payout</h1>
+    <h1 class="w-100 p-4 text-center">Willkommen bei der Auszahlungsseite!</h1>
+    <h4 class="w-100 text-center">Um fortzufahren bitten einscannen:</h4>
+    <img id="qrcode" class="position-absolute start-50 top-50 translate-middle" :src="'https://api.qrserver.com/v1/create-qr-code/?size=250x250&data=notify/'+code">
 </template>
 
 <style scoped lang="scss">
+#qrcode {
 
+}
 </style>
