65b12ce27272f14f7d91f71b28b7405009afbdce
Jonas Pfeifer <jonaspfeifer@drei.at>
2023-11-05 00:58:45 +0100

    admin page done

--- src/views/AdminView.vue
+++ src/views/AdminView.vue
@@ -7,6 +7,7 @@ const backendIP = inject("backendIP");
 interface User {
     id: string,
     name: string,
+    points: number,
 }
 
 let users = ref([] as User[]);
@@ -18,12 +19,18 @@ function loadUsers() {
         users.value = userList.data;
     })
 }
+
+function removeUser(id) {
+    axios.delete(`${backendIP}user/${id}`).then(() => {
+        loadUsers();
+    })
+}
 </script>
 
 <template>
     <h1 class="w-100 p-3 mb-5 text-center">THIS PAGE IS ONLY FOR ADMINS!!</h1>
-    <div class="d-flex flex-column align-items-center gap-3">
-        <div v-for="user in users" :key="user.id">{{user.name}}: {{user.id}}</div>
+    <div class="d-flex p-4 flex-column align-items-center gap-3">
+        <div v-for="user in users" :key="user.id">{{user.name}}: {{user.id}} {{user.points}} <button class="text-black" @click="removeUser(user.id)">x</button></div>
     </div>
 </template>
 
